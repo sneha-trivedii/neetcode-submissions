b84f951c9b17e515731e625fe8efb989b9fe8f3e
Sneha <142036057+sneha-trivedii@users.noreply.github.com>
2026-08-25 02:56:49 +0530

    Remove: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevNums = {}
-        for i in range(len(nums)):
-            if (target - nums[i]) in prevNums:
-                return [prevNums[target - nums[i]], i]
-            else:
-                prevNums[nums[i]] = i
